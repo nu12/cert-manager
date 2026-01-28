a3192f3b1abbf9adbce955a5415bfa58a53d51c9
nu12 <34694287+nu12@users.noreply.github.com>
2026-01-27 19:35:14 -0500

    Bootstrap authentication

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  include Yaag::Test::SessionsHelper
+  setup { sign_in_as(User.take) }
+  
+  test "should get index" do
+    get home_index_url
+    assert_response :success
+  end
+end
