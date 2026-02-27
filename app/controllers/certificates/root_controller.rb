1df8b875bf6b68fc48c368b96617a7afc6ea1ba6
nu12 <34694287+nu12@users.noreply.github.com>
2026-02-27 14:33:34 -0500

    Refactor

--- app/controllers/certificates/root_controller.rb
+++ app/controllers/certificates/root_controller.rb
@@ -1,24 +1,41 @@
 class Certificates::RootController < ApplicationController
+  before_action :set_certificate, only: %i[ show ]
+  before_action :certificate_params, only: %i[ create ]
   def index
   end
 
   def show
-    @certificate = Certificate.find(params[:id])
   end
 
   def new
-    @certificate = Certificate.new
+    @c, @st, @l, @o, @ou, @cn, @size, @password = ""
   end
 
   def create
-    params.expect([ :country, :state, :location, :organization, :organization_unit, :common_name, :key_size ])
     begin
-      key = Key.create!(content: CertManager::Key.create(params[:key_size].to_i, params[:password]), user: current_user)
-      certificate = Certificate.create!(content: CertManager::Certificate.create_root(CertManager::Key.parse(key, params[:password]), "/C=#{params[:country]}/ST=#{params[:state]}/L=#{params[:location]}/O=#{params[:organization]}/OU=#{params[:organization_unit]}/CN=#{params[:common_name]}"), name: params[:common_name], user: current_user, key: key)
+      key = Key.create!(content: CertManager::Key.create(@size.to_i, @password), user: current_user)
+      certificate = Certificate.create!(content: CertManager::Certificate.create_root(CertManager::Key.parse(key, @password), "/C=#{@c}/ST=#{@st}/L=#{@l}/O=#{@o}/OU=#{@ou}/CN=#{@cn}"), name: @cn, user: current_user, key: key)
       redirect_to certificates_root_path(certificate), notice: "Certificate was successfully created."
     rescue => error
       @error = error.message
       render :new, status: :unprocessable_entity
     end
   end
+  private
+    def set_certificate
+      params.expect(:id)
+      @certificate = Certificate.find(params[:id])
+      # authorize @certificate
+    end
+    def certificate_params
+      params.expect([ :country, :state, :location, :organization, :organization_unit, :common_name, :key_size ])
+      @c = params[:country]
+      @st = params[:state]
+      @l = params[:location]
+      @o = params[:organization]
+      @ou = params[:organization_unit]
+      @cn = params[:common_name]
+      @size = params[:key_size]
+      @password = params[:password]
+    end
 end
