ed1f967c064f5994cc88d6aeebb6bba4ed238d0d
nu12 <34694287+nu12@users.noreply.github.com>
2026-02-28 12:24:34 -0500

    Add validity

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -4,4 +4,8 @@ class Certificate < ApplicationRecord
   belongs_to :certificate, optional: true
   has_many :children, class_name: "Certificate", foreign_key: "certificate_id"
   belongs_to :parent, class_name: "Certificate", foreign_key: "certificate_id", optional: true
+
+  def is_valid?
+    self.expired_at >= DateTime.now
+  end
 end
