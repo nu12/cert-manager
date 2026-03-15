f149f08655369733ba7c2ce5ab903c4b5c802ac4
nu12 <34694287+nu12@users.noreply.github.com>
2026-03-15 07:44:46 -0400

    Add toast

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load',() => {
+  const toastElList = document.querySelectorAll('.toast')
+  toastElList.forEach(toastEl => {
+    new bootstrap.Toast(toastEl, {"autohide": true}).show()
+  })
+})
