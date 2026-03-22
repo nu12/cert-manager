c80f584cf52e2cc0eb231d0913b4fa4978cea59d
nu12 <34694287+nu12@users.noreply.github.com>
2026-03-22 19:32:20 -0400

    Intermediate certificate creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,19 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+    def certificate_params
+      params.expect([ :country, :state, :location, :organization, :organization_unit, :common_name, :key_size, :validity ])
+      @authority_password = params[:authority_password]
+      @c = params[:country]
+      @st = params[:state]
+      @l = params[:location]
+      @o = params[:organization]
+      @ou = params[:organization_unit]
+      @cn = params[:common_name]
+      @size = params[:key_size]
+      @password = params[:password]
+      @validity = params[:validity]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :ca, :controller => "certificates/root", :alias => "root"
   namespace :certificates do
     resources :root, only: [ :index, :show, :new, :create ] do
-      resources :intermediate, only: [ :show ] do
+      resources :intermediate, only: [ :show, :new, :create ] do
         resources :server, only: [ :show ]
       end
     end
