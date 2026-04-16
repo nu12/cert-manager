9a4ad518b2163bd4ca2edcadf291301c0ce05592
nu12 <34694287+nu12@users.noreply.github.com>
2026-04-16 08:44:32 -0400

    Fix system test

--- test/system/renew_test.rb
+++ test/system/renew_test.rb
@@ -1,20 +1,35 @@
 require "application_system_test_case"
 
 class RenewTest < ApplicationSystemTestCase
-  test "renew" do
+  test "root" do
     visit create_session_url(users(:one).email_address_login_token)
     click_on certificates(:root).common_name
+    assert_content certificates(:root).common_name
     click_on "Renew"
     fill_in "Expirity date", with: "01-01-2030"
     click_on "Save"
     assert_content "Root certificate was successfully created"
+  end
 
-    visit renew_url(certificates(:intermediate))
+  test "intermediate" do
+    visit create_session_url(users(:one).email_address_login_token)
+    click_on certificates(:root).common_name
+    click_on certificates(:intermediate).common_name
+    assert_content certificates(:intermediate).common_name
+    assert_content "Intermediate certificate details (#{certificates(:intermediate).common_name})"
+    click_on "Renew"
     fill_in "Expirity date", with: "01-01-2030"
     click_on "Save"
     assert_content "Intermediate certificate was successfully created"
+  end
 
-    visit renew_url(certificates(:server))
+  test "server" do
+    visit create_session_url(users(:one).email_address_login_token)
+    click_on certificates(:root).common_name
+    click_on certificates(:intermediate).common_name
+    click_on certificates(:server).common_name, match: :first
+    assert_content "Server certificate details (#{certificates(:server).common_name})"
+    click_on "Renew"
     fill_in "Expirity date", with: "01-01-2030"
     click_on "Save"
     assert_content "Server certificate was successfully created"
